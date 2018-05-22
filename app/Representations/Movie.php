a9bc18a868a788f2258d03f56bf7300d4d720643
ValentinNikolaev <valenti.nikolaev@gmail.com>
2018-05-22 16:54:26 +0300

    first part movie create representation

--- app/Representations/Movie.php
+++ app/Representations/Movie.php
@@ -15,7 +15,41 @@ class Movie extends Representation
     public function create()
     {
         return [
-        
+            /**
+             * @api-data    external_urls (object) - External URLs
+             * @api-version >=1.0
+             * @api-see     self::getExternalUrls external_urls
+             */
+            'external_urls' => $this->getExternalUrls(),
+        ];
+    }
+    
+    
+    /**
+     * @return array
+     */
+    private function getExternalUrls()
+    {
+        return [
+            /**
+             * @api-data trailer (string) - Trailer URL
+             */
+            'trailer'  => $this->movie->trailer,
+            /**
+             * @api-data trailer (array) - Trailers URLs
+             */
+            'trailers' => $this->movie->trailers,
+            /**
+             * @api-data    tickets (string, BUY_TICKETS) - Tickets URL
+             * @api-version <1.2.1
+             */
+            'tickets'  => $this->movie->tickets_url,
+            
+            /**
+             * @api-data    imdb (string) - imdb URL
+             * @api-version >1.0.0
+             */
+            'imdb'     => $this->movie->imdb,
         ];
     }
 }
